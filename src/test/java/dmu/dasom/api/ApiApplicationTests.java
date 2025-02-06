7fc789295c44db0bae9d8ee7017dee71249c9e58
Seungwan Yoo <ysw99@kakao.com>
2025-02-06 13:52:17 +0900

    chore: Initial Commit

---
+++ src/test/java/dmu/dasom/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dmu.dasom.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

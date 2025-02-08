a33352eeff5fe058f1c6336a30bba4a0b0182ae6
Seungwan Yoo <ysw99@kakao.com>
2025-02-08 16:56:02 +0900

    feat: ControllerAdvice 예외 처리 추가 - 객체 필드 타입 검증 예외 Handler 추가

--- src/main/java/dmu/dasom/api/domain/common/exception/CustomControllerAdvice.java
+++ src/main/java/dmu/dasom/api/domain/common/exception/CustomControllerAdvice.java
@@ -1,6 +1,8 @@
 package dmu.dasom.api.domain.common.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -10,6 +12,11 @@ public class CustomControllerAdvice {
     @ExceptionHandler(CustomException.class)
     public ResponseEntity<ErrorResponse> customException(final CustomException e) {
         return ResponseEntity.status(e.getErrorCode().getStatus()).body(new ErrorResponse(e.getErrorCode()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> methodArgumentNotValidException(final MethodArgumentNotValidException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(ErrorCode.ARGUMENT_NOT_VALID));
     }
 
 }

b602d5f3a8ddcee06686fc52c277a1659699f81c
Seungwan Yoo <ysw99@kakao.com>
2025-02-08 16:17:41 +0900

    feat: Security FilterChain 예외 처리 구현 - ErrorResponse 타입으로 예외 응답

---
+++ src/main/java/dmu/dasom/api/global/auth/handler/AccessDeniedHandlerImpl.java
@@ -0,0 +1,33 @@
+package dmu.dasom.api.global.auth.handler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dmu.dasom.api.domain.common.exception.ErrorCode;
+import dmu.dasom.api.domain.common.exception.ErrorResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class AccessDeniedHandlerImpl implements AccessDeniedHandler {
+
+    private final ObjectMapper objectMapper;
+
+    public AccessDeniedHandlerImpl(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException {
+        response.setStatus(HttpStatus.FORBIDDEN.value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(objectMapper.writeValueAsString(new ErrorResponse(ErrorCode.FORBIDDEN)));
+    }
+
+}

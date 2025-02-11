253926f2b41ec1e51c0308a4f2dcc36c4feddd1b
Seungwan Yoo <ysw99@kakao.com>
2025-02-12 00:32:00 +0900

    feat: 어드민; 지원자 조회 엔드포인트 구현

---
+++ src/main/java/dmu/dasom/api/global/admin/controller/AdminController.java
@@ -0,0 +1,52 @@
+package dmu.dasom.api.global.admin.controller;
+
+import dmu.dasom.api.domain.applicant.dto.ApplicantResponseDto;
+import dmu.dasom.api.domain.applicant.service.ApplicantService;
+import dmu.dasom.api.global.dto.PageResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.validation.constraints.Min;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/admin")
+@RequiredArgsConstructor
+public class AdminController {
+
+    private final ApplicantService applicantService;
+
+    // 지원자 조회
+    @Operation(summary = "지원자 조회")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "지원자 조회 성공"),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class),
+                            examples = {
+                                    @ExampleObject(
+                                            name = "조회 결과 없음",
+                                            value = "{ \"code\": \"C011\", \"message\": \"조회 결과가 없습니다.\" }"
+                                    )
+                            }
+                    )
+            )
+    })
+    @GetMapping("/applicants")
+    public ResponseEntity<PageResponse<ApplicantResponseDto>> getApplicants(
+            @RequestParam(value = "page", defaultValue = "0") @Min(0) final int page
+    ) {
+        return ResponseEntity.ok(applicantService.getApplicants(page));
+    }
+
+}

cf2dc7a44df06c845afd6fb47b5d09754e21b7d2
Seungwan Yoo <ysw99@kakao.com>
2025-02-12 00:30:38 +0900

    feat: 지원자 조회 로직 구현 - 페이징 정책에 따라 20건 씩 응답

--- src/main/java/dmu/dasom/api/domain/applicant/service/ApplicantService.java
+++ src/main/java/dmu/dasom/api/domain/applicant/service/ApplicantService.java
@@ -1,9 +1,13 @@
 package dmu.dasom.api.domain.applicant.service;
 
 import dmu.dasom.api.domain.applicant.dto.ApplicantCreateRequestDto;
+import dmu.dasom.api.domain.applicant.dto.ApplicantResponseDto;
+import dmu.dasom.api.global.dto.PageResponse;
 
 public interface ApplicantService {
 
     void apply(final ApplicantCreateRequestDto request);
+
+    PageResponse<ApplicantResponseDto> getApplicants(final int page);
 
 }
--- src/main/java/dmu/dasom/api/domain/applicant/service/ApplicantServiceImpl.java
+++ src/main/java/dmu/dasom/api/domain/applicant/service/ApplicantServiceImpl.java
@@ -1,13 +1,22 @@
 package dmu.dasom.api.domain.applicant.service;
 
 import dmu.dasom.api.domain.applicant.dto.ApplicantCreateRequestDto;
+import dmu.dasom.api.domain.applicant.dto.ApplicantResponseDto;
+import dmu.dasom.api.domain.applicant.entity.Applicant;
 import dmu.dasom.api.domain.applicant.repository.ApplicantRepository;
+import dmu.dasom.api.domain.common.exception.CustomException;
+import dmu.dasom.api.domain.common.exception.ErrorCode;
+import dmu.dasom.api.global.dto.PageResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class ApplicantServiceImpl implements ApplicantService {
+
+    private final static int DEFAULT_PAGE_SIZE = 20;
 
     private final ApplicantRepository applicantRepository;
 
@@ -15,6 +24,18 @@ public class ApplicantServiceImpl implements ApplicantService {
     @Override
     public void apply(final ApplicantCreateRequestDto request) {
         applicantRepository.save(request.toEntity());
+    }
+
+    // 지원자 조회
+    @Override
+    public PageResponse<ApplicantResponseDto> getApplicants(final int page) {
+        final Page<Applicant> applicants = applicantRepository.findAllWithPageRequest(PageRequest.of(page, DEFAULT_PAGE_SIZE));
+
+        // 조회 조건에 따라 결과가 없을 수 있음
+        if (applicants.isEmpty())
+            throw new CustomException(ErrorCode.EMPTY_RESULT);
+
+        return PageResponse.from(applicants.map(Applicant::toApplicantResponse));
     }
 
 }

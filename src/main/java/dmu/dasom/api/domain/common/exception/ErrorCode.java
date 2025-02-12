45020d953e8a5ebccc3dac89cbb4384bf92a91c1
Seungwan Yoo <ysw99@kakao.com>
2025-02-12 14:36:48 +0900

    fix: 에러 코드 수정 - 병합 충돌을 막기 위해 에러 코드 수정

--- src/main/java/dmu/dasom/api/domain/common/exception/ErrorCode.java
+++ src/main/java/dmu/dasom/api/domain/common/exception/ErrorCode.java
@@ -17,7 +17,7 @@ public enum ErrorCode {
     TOKEN_NOT_VALID(400, "C008", "토큰이 올바르지 않습니다."),
     INTERNAL_SERVER_ERROR(500, "C009", "서버에 문제가 발생하였습니다."),
     NOT_FOUND(404, "C010", "해당 리소스를 찾을 수 없습니다."),
-    EMPTY_RESULT(400, "C011", "조회 결과가 없습니다.")
+    EMPTY_RESULT(400, "C012", "조회 결과가 없습니다.")
     ;
 
     private final int status;

8b0ef75e43f687a11e66eda16f21bb39f2b80305
Seungwan Yoo <ysw99@kakao.com>
2025-02-13 23:05:45 +0900

    feat: 지원자 학번 중복 불가 설정
    * feat: 지원자 정보 갱신 (덮어쓰기) 메소드 구현, studentNo Unique 제약조건 추가
    
    * feat: 지원자 생성 요청 DTO 수정
    - 누락되었던 name 필드 추가
    - 덮어쓰기 확인 여부 필드 추가 (기본값 false, nullable)
    
    * feat: 지원자 저장 로직 학번 검증 추가
    - 이미 지원한 지원자가 다시 지원 시 덮어쓰기 하거나 저장하지 않는 방식으로 구현
    - 에러코드 추가
    
    * test: 변경된 저장 로직에 따라 테스트 케이스 구현
    
    * docs: API 명세 실패 케이스 추가

--- src/main/java/dmu/dasom/api/domain/common/exception/ErrorCode.java
+++ src/main/java/dmu/dasom/api/domain/common/exception/ErrorCode.java
@@ -18,7 +18,8 @@ public enum ErrorCode {
     INTERNAL_SERVER_ERROR(500, "C009", "서버에 문제가 발생하였습니다."),
     NOT_FOUND(404, "C010", "해당 리소스를 찾을 수 없습니다."),
     WRITE_FAIL(400, "C011", "데이터를 쓰는데 실패하였습니다."),
-    EMPTY_RESULT(400, "C012", "조회 결과가 없습니다.")
+    EMPTY_RESULT(400, "C012", "조회 결과가 없습니다."),
+    DUPLICATED_STUDENT_NO(400, "C013", "이미 등록된 학번입니다."),
     ;
 
     private final int status;
--- src/main/java/dmu/dasom/api/domain/recruit/controller/RecruitController.java
+++ src/main/java/dmu/dasom/api/domain/recruit/controller/RecruitController.java
@@ -2,7 +2,11 @@ package dmu.dasom.api.domain.recruit.controller;
 
 import dmu.dasom.api.domain.applicant.dto.ApplicantCreateRequestDto;
 import dmu.dasom.api.domain.applicant.service.ApplicantService;
+import dmu.dasom.api.domain.common.exception.ErrorResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
@@ -23,8 +27,15 @@ public class RecruitController {
     // 지원하기
     @Operation(summary = "부원 지원하기")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "지원 성공")
-    })
+            @ApiResponse(responseCode = "200", description = "지원 성공"),
+            @ApiResponse(responseCode = "400", description = "실패 케이스",
+                content = @Content(
+                        mediaType = "application/json",
+                        schema = @Schema(implementation = ErrorResponse.class),
+                        examples = {
+                            @ExampleObject(
+                                name = "학번 중복",
+                                value = "{ \"code\": \"C013\", \"message\": \"이미 등록된 학번입니다.\" }")}))})
     @PostMapping("/apply")
     public ResponseEntity<Void> apply(@Valid @RequestBody final ApplicantCreateRequestDto request) {
         applicantService.apply(request);

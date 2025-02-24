7f3403265627b2fa1fb335cfa36c60f43806019e
hodoon <ehgns79513@gmail.com>
2025-02-24 11:45:08 +0900

    fix: 발송 이메일 템플릿 수정

--- src/main/java/dmu/dasom/api/domain/email/service/EmailService.java
+++ src/main/java/dmu/dasom/api/domain/email/service/EmailService.java
@@ -28,25 +28,36 @@ public class EmailService {
         }
         // 메일 제목 및 템플릿 설정
         String subject;
-        String templateName = switch (mailType) {
+        String emailContent;
+        String buttonUrl = "#";
+        String buttonText;
+        switch (mailType) {
             case DOCUMENT_RESULT -> {
-
                 subject = "동양미래대학교 컴퓨터소프트웨어공학과 전공 동아리 DASOM 서류 결과 안내";
-                yield "document-pass-template";
+                emailContent = "먼저 다솜 34기에 많은 관심을 갖고 지원해 주셔서 감사드리며,<br>" +
+                        "내부 서류 평가 결과 및 추후 일정에 관해 안내드리고자 이메일을 발송하게 되었습니다.<br>" +
+                        "모집 폼 합/불합 결과는 아래 버튼 혹은 홈페이지를 통해 확인이 가능합니다.";
+                buttonText = "서류 결과 확인하기";
             }
             case FINAL_RESULT -> {
                 subject = "동양미래대학교 컴퓨터소프트웨어공학과 전공 동아리 DASOM 최종 합격 안내";
-                yield "final-pass-template";
+                emailContent = "먼저 다솜 34기에 많은 관심을 갖고 지원해 주셔서 감사드리며,<br>" +
+                        "최종 면접 결과 및 추후 일정에 관해 안내드리고자 이메일을 발송하게 되었습니다.<br>" +
+                        "모집 폼 합/불합 결과는 아래 버튼 혹은 홈페이지를 통해 확인이 가능합니다.";
+                buttonText = "최종 결과 확인하기";
             }
             default -> throw new IllegalStateException("Unexpected value: " + mailType);
-        };
+        }
 
         // HTML 템플릿에 전달할 데이터 설정
         Context context = new Context();
         context.setVariable("name", name); // 지원자 이름 전달
+        context.setVariable("emailContent", emailContent); // 이메일 내용 전달
+        context.setVariable("buttonUrl", buttonUrl); // 버튼 링크 전달
+        context.setVariable("buttonText", buttonText);
 
         // HTML 템플릿 처리
-        String htmlBody = templateEngine.process(templateName, context);
+        String htmlBody = templateEngine.process("email-template", context);
 
         // 이메일 생성 및 전송
         MimeMessage message = javaMailSender.createMimeMessage();

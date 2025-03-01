043fe4b6f9217d77f1d63689ba807a6037ed1676
Seungwan Yoo <ysw99@kakao.com>
2025-03-01 10:55:18 +0900

    feat: fileType, targetId로 파일 조회 구현

--- src/main/java/dmu/dasom/api/global/file/repository/FileRepository.java
+++ src/main/java/dmu/dasom/api/global/file/repository/FileRepository.java
@@ -1,14 +1,22 @@
 package dmu.dasom.api.global.file.repository;
 
 import dmu.dasom.api.global.file.entity.FileEntity;
+import dmu.dasom.api.global.file.enums.FileType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface FileRepository extends JpaRepository<FileEntity, Long> {
 
-    List<FileEntity> findAllById(Iterable<Long> ids);
+    List<FileEntity> findByFileTypeAndTargetId(FileType fileType, Long targetId);
+
+    @Query("SELECT f FROM FileEntity f WHERE f.fileType = :fileType AND f.targetId = :targetId")
+    Optional<FileEntity> findFirstByFileTypeAndTargetIds(@Param("fileType") FileType fileType,
+                                             @Param("targetId") Long targetId);
 
 }

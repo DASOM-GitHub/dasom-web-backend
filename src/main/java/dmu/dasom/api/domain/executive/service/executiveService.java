d78786453ca17f0fb0061e7412dc9852c07734c9
si-zero <snm306@naver.com>
2025-08-20 15:46:49 +0900

    feat: 회장단 서비스 생성

---
+++ src/main/java/dmu/dasom/api/domain/executive/service/executiveService.java
@@ -0,0 +1,20 @@
+package dmu.dasom.api.domain.executive.service;
+
+import dmu.dasom.api.domain.executive.dto.executiveCreationResponseDto;
+import dmu.dasom.api.domain.executive.repository.executiveRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true) // 기본값 : 읽기 전용
+public class executiveService {
+
+    private final executiveRepository executiveRepository;
+
+    // 회장단 멤버 생성
+    /*public executiveCreationResponseDto createExecutive(executiveCreationResponseDto requestDto) {
+        return new executiveCreationResponseDto(executiveRepository.save(requestDto.toEntity()).getId());
+    }*/
+}

62149c599d83a91afe85b1dc8670c974766ea228
si-zero <snm306@naver.com>
2025-08-20 16:02:35 +0900

    feat: 임원진 멤버 생성 서비스 기능 추가

--- src/main/java/dmu/dasom/api/domain/executive/service/ExecutiveService.java
+++ src/main/java/dmu/dasom/api/domain/executive/service/ExecutiveService.java
@@ -1,5 +1,7 @@
 package dmu.dasom.api.domain.executive.service;
 
+import dmu.dasom.api.domain.executive.dto.ExecutiveCreationResponseDto;
+import dmu.dasom.api.domain.executive.dto.ExecutiveRequestDto;
 import dmu.dasom.api.domain.executive.repository.ExecutiveRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,7 +15,7 @@ public class ExecutiveService {
     private final ExecutiveRepository executiveRepository;
 
     // 회장단 멤버 생성
-    /*public executiveCreationResponseDto createExecutive(executiveCreationResponseDto requestDto) {
-        return new executiveCreationResponseDto(executiveRepository.save(requestDto.toEntity()).getId());
-    }*/
+    public ExecutiveCreationResponseDto createExecutive(ExecutiveRequestDto requestDto) {
+        return new ExecutiveCreationResponseDto(executiveRepository.save(requestDto.toEntity()).getId());
+    }
 }

f88c066387edc533a574e5997dfa201a67e8031d
si-zero <snm306@naver.com>
2025-08-22 10:53:40 +0900

    rename: 파일이름 오탈자 수정

--- src/main/java/dmu/dasom/api/domain/executive/controller/ExecutiveController.java
+++ src/main/java/dmu/dasom/api/domain/executive/controller/ExecutiveController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/executives")
-public class ExecutiveContorller {
+public class ExecutiveController {
 
     private final ExecutiveService executiveService;
 

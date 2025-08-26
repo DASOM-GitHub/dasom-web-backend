87a118760d9b25e3de08313b4062c03de8abdcd7
jucheonsu <jucheonsu@gmail.com>
2025-08-26 13:40:35 +0900

    rename: Activity로 변경 (DASOMBE-15)

--- src/main/java/dmu/dasom/api/domain/activity/repository/ActivityHistoryRepository.java
+++
@@ -1,8 +0,0 @@
-package dmu.dasom.api.domain.activity.repository;
-
-import dmu.dasom.api.domain.activity.entity.ActivityHistory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ActivityHistoryRepository extends JpaRepository<ActivityHistory, Long> {
-
-}

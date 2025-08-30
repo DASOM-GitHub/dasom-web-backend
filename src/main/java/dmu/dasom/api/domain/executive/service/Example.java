bc092319dea643b5c4450b136a4861b4af15f3ca
si-zero <snm306@naver.com>
2025-08-31 00:22:37 +0900

    fix: 오류 수정

---
+++ src/main/java/dmu/dasom/api/domain/executive/service/Example.java
@@ -0,0 +1,9 @@
+package dmu.dasom.api.domain.executive.service;
+
+public class Example {
+    public static void main(String[] args) {
+        String a = "김다솜";
+        String b = "김다솜";
+        System.out.println(a.equals(b));
+    }
+}

2ec7fb6a6f7428136ce4c6bf94e065033038d3a8
Dohyeon Choi <do.hy824@gmail.com>
2025-08-31 01:59:16 +0900

    hotfix: setter 임포트문 추가

--- src/main/java/dmu/dasom/api/domain/executive/dto/ExecutiveRequestDto.java
+++ src/main/java/dmu/dasom/api/domain/executive/dto/ExecutiveRequestDto.java
@@ -7,6 +7,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 import lombok.NoArgsConstructor;
 
 @Getter
